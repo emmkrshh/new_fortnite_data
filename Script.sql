18189c1cdf73caf1951493dd0eaa2236036eebd3
emmkrshh <160784950+emmkrshh@users.noreply.github.com>
2025-03-02 07:25:20 -0600

    Update and rename Script.sql to BLANK DO NOT USE

--- Script.sql
+++
@@ -1,7 +0,0 @@
-/kaggle/input/grocery-sales-dataset/countries.csv
-/kaggle/input/grocery-sales-dataset/products.csv
-/kaggle/input/grocery-sales-dataset/categories.csv
-/kaggle/input/grocery-sales-dataset/cities.csv
-/kaggle/input/grocery-sales-dataset/employees.csv
-/kaggle/input/grocery-sales-dataset/sales.csv
-/kaggle/input/grocery-sales-dataset/customers.csv
